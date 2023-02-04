b4578d06bc85f941fe144a32b31004603bece839
Tertha Dev Sarker <devteertha28@gmail.com>
2023-02-05 02:13:43 +0600

    new push

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useSelector } from "react-redux";
+
+interface IProductInfo {
+  isLoading: boolean;
+  error: string;
+  products: any[];
+}
 
 const Products = () => {
-    const ProductsInfo = useSelector((state) => (state));
-    console.log(ProductsInfo.isLoading);
-    
-    return (
-        <div>
-            <h1>Hello Products</h1>
-        </div>
-    );
+  const productsInfo = useSelector((state: IProductInfo) => state);
+  const { isLoading, error, products } = productsInfo;
+  console.log("productsInfo: ", productsInfo);
+
+  return (
+    <div>
+      <h1>Hello Products</h1>
+    </div>
+  );
 };
 
 export default Products;
