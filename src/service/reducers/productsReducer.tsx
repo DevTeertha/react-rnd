93e28171066e496127b6e0ca5c4e7c1453d2dc3a
Tertha Dev Sarker <devteertha28@gmail.com>
2023-02-05 02:13:43 +0600

    new push

--- src/service/reducers/productsReducer.tsx
+++ src/service/reducers/productsReducer.tsx
@@ -1,37 +1,37 @@
-import { GET_PRODUCTS_FAILED, GET_PRODUCTS_REQUEST, GET_PRODUCTS_SUCCESS } from "../constants/productsConstant";
+import {
+  GET_PRODUCTS_FAILED,
+  GET_PRODUCTS_REQUEST,
+  GET_PRODUCTS_SUCCESS,
+} from "../constants/productsConstant";
 
 const initialState = {
-    isLoading : false,
-    products: [],
-    error: null,
-}
+  isLoading: false,
+  products: [],
+  error: null,
+};
 
-const productsReducer = (state = initialState, action:any)=>{
-    switch (action.type) {
-        case GET_PRODUCTS_REQUEST:
-            
-            return{
-                ...state,
-                isLoading: true,
-            }
-        case GET_PRODUCTS_SUCCESS:
-        
-            return{
-                isLoading : false,
-                products: action.payload,
-                error: null,
-
-            }
-        case GET_PRODUCTS_FAILED:
-            
-            return{
-                isLoading : false,
-                products: [],
-                error: action.payload,
-            }
-        default:
-            return state;
-    }
-}
+const productsReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case GET_PRODUCTS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        isLoading: false,
+        products: action.payload,
+        error: null,
+      };
+    case GET_PRODUCTS_FAILED:
+      return {
+        isLoading: false,
+        products: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default productsReducer;
