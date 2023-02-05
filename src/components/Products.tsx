83b5f0be48f85cccd64dbfa426808083eab0ae08
anik-kumaar-datta <anikkumaardatta@gmail.com>
2023-02-06 02:51:35 +0600

    getting all data from API

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts } from "../service/actions/productsAction";
 
 interface IProductInfo {
   isLoading: boolean;
@@ -10,11 +12,26 @@ interface IProductInfo {
 const Products = () => {
   const productsInfo = useSelector((state: IProductInfo) => state);
   const { isLoading, error, products } = productsInfo;
-  console.log(productsInfo);
+  
+  const dispatch: any = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts())
+  }, [])
 
   return (
     <div>
       <h1>Hello Products</h1>
+      {isLoading && <h2>Is Loading ....</h2>}
+      {error && <h2>error.message</h2>}
+      <section>
+        {products && products.map((product) => {
+          return <article>
+            <h2>{product.id}</h2>
+            <h3>{product.title}</h3>
+          </article>
+        })}
+      </section>
     </div>
   );
 };
