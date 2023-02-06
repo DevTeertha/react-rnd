4afe2add9cc264cf0b981255d77e1d6ed5d7863d
anik-kumaar-datta <anikkumaardatta@gmail.com>
2023-02-06 21:17:40 +0600

    share data components

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../service/actions/productsAction";
+import ProductCard from "../components/Cards/ProductCard"
 
 interface IProductInfo {
   isLoading: boolean;
@@ -20,17 +21,17 @@ const Products = () => {
   }, [])
 
   return (
-    <div>
-      <h1>Hello Products</h1>
+    <div className="max-w-6xl mx-auto my-20">
       {isLoading && <h2>Is Loading ....</h2>}
       {error && <h2>error.message</h2>}
-      <section>
-        {products && products.map((product) => {
-          return <article>
-            <h2>{product.id}</h2>
-            <h3>{product.title}</h3>
-          </article>
-        })}
+      <h1 className="text-5xl font-bold text-center">Products </h1>
+      <section className="m-5 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {products && products.map ((product): any => (
+          <ProductCard
+          key={product.id}
+          product={product}
+          ></ProductCard>
+        ))}
       </section>
     </div>
   );
